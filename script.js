f639d693eafb92aed96b93b8fe79c91e3ea6f3b9
Ferpiz445 <89951811+Ferpiz445@users.noreply.github.com>
2024-09-07 02:37:54 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Funções para scroll da nav
+
+document.getElementById("home-buttom").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById("home-tela").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("skills-buttom").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById("skills-tela").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("certificados-buttom").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById("certificados-tela").scrollIntoView({behavior: "smooth"});
+}
+
