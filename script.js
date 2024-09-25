1755e65814f2147ff6c0ca39c3d6ae6dd4f9f7fa
Ferpiz445 <89951811+Ferpiz445@users.noreply.github.com>
2024-09-24 23:03:42 -0300

    mutia coisa!!! biblioteca, ajuste navbar, parallax nos heros e muito mais!

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.onclick = function () {
+        menu.classList.toggle('active');
+    };
+
 //Funções para scroll da nav
 
 document.getElementById("logo-buttom").onclick = function (event) {
@@ -10,13 +17,25 @@ document.getElementById("home-buttom").onclick = function (event) {
     document.getElementById("home-tela").scrollIntoView({behavior: "smooth"});
 }
 
-document.getElementById("skills-buttom").onclick = function (event) {
+document.getElementById("about-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("skills-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("about-tela").scrollIntoView({behavior: "smooth"});
 }
 
 document.getElementById("certificados-buttom").onclick = function (event) {
     event.preventDefault();
     document.getElementById("certificados-tela").scrollIntoView({behavior: "smooth"});
 }
+
+document.getElementById("skills-buttom").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById("skills-tela").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("contatos-buttom").onclick = function (event) {
+    event.preventDefault();
+    document.getElementById("contatos-tela").scrollIntoView({behavior: "smooth"});
+}
+
+
 
