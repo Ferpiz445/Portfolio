2675aab6369ccf4071d4b987e485f0a703e2435e
Ferpiz445 <89951811+Ferpiz445@users.noreply.github.com>
2024-10-03 18:53:30 -0300

    ajustes nos contatos e remoção do nivel das skills

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
+// Hamburguer
 
-    menuToggle.onclick = function () {
-        menu.classList.toggle('active');
-    };
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
 
-//Funções para scroll da nav
+menuToggle.onclick = function () {
+    menu.classList.toggle('active');
+};
+
+// Funções para scroll da nav
 
 document.getElementById("logo-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("home-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("home-tela").scrollIntoView({ behavior: "smooth" });
 }
 
 document.getElementById("home-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("home-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("home-tela").scrollIntoView({ behavior: "smooth" });
 }
 
 document.getElementById("about-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("about-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("about-tela").scrollIntoView({ behavior: "smooth" });
 }
 
 document.getElementById("certificados-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("certificados-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("certificados-tela").scrollIntoView({ behavior: "smooth" });
 }
 
 document.getElementById("skills-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("skills-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("skills-tela").scrollIntoView({ behavior: "smooth" });
 }
 
 document.getElementById("contatos-buttom").onclick = function (event) {
     event.preventDefault();
-    document.getElementById("contatos-tela").scrollIntoView({behavior: "smooth"});
+    document.getElementById("contatos-tela").scrollIntoView({ behavior: "smooth" });
 }
 
+// Contatos 
 
+document.getElementById("whatsapp").onclick = function () {
+    window.open("https://wa.me//5511943536373" , "_blank")
+}
+
+document.getElementById("linkedin").onclick = function () {
+    window.open("https://www.linkedin.com/in/felipe-monteiro-a1ba1620a/" , "_blank")
+}
+
+document.getElementById("gmail").onclick = function () {
+    window.open("mailto:fe.monteiro2002@gmail.com" , "_blank")
+}
+
+document.getElementById("github-ctt").onclick = function () {
+    window.open("https://github.com/Ferpiz445" , "_blank")
+}
 
